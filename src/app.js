2137f2073988e13c4e0b7378e194874bbd54d550
Fernanda Bernardo <fernandabernardo94@gmail.com>
2020-10-26 19:57:42 -0300

    simple fix

--- src/app.js
+++ src/app.js
@@ -3,13 +3,20 @@ const app = express();
 const path = require('path');
 const schedule = require('./buildSchedule.js');
 
+const PORT = process.env.PORT || 5000;
+
 app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
+  res.send("Hello Scalonator :)")
+})
+
+app.get('/schedule', function (req, res) {
   res.render('index', {
     schedule: schedule.build().schedule
   });
 })
-app.listen(3000);
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`));
